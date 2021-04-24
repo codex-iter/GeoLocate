162031f060801087d6af7e087847364926f7b734
Bravish Ghosh <grandson.baba.2012@gmail.com>
2021-04-24 16:51:43 -0700

    Added hospital data

---
+++ hospital-data.js
@@ -0,0 +1,566 @@
+let hospitals = [{
+    "name": "Lok Nayak Hospital",
+    "phoneNumber":"011 2323 6000",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.6383,
+        "longitude": 77.2386, 
+    },
+
+    "address":"near, metro Station Central, Jawaharlal Nehru Marg, Delhi Gate, New Delhi, 110002",
+    "addressLines":[
+        "near, metro Station Central", 
+        "Jawaharlal Nehru Marg", 
+        "Delhi Gate", 
+        "New Delhi, 110002"],
+    "pincode": "110002",
+    "zipCode":"110002",
+    "numBeds" : "2000",
+    "rating" : "3.6",
+},
+
+{
+    "name": "Guru Teg Bahadur Hospital",
+    "phoneNumber":"011 2258 6262",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.6840,
+        "longitude": 77.3094,
+    },
+    "address":"Tahirpur Rd, GTB Enclave, Dilshad Garden, New Delhi, Delhi 110095",
+    "addressLines":["Tahirpur Rd, GTB Enclave", "Dilshad Garden", "Delhi 110095"],
+    "pincode": "110095",
+    "zipCode":"110095",
+    "numBeds" : "1500",
+    "rating" : "3.5",
+},
+
+{
+    "name": "Venkateshwar Hospital",
+    "phoneNumber":"011 4855 5555",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.5889,
+        "longitude": 77.0375,
+    },
+    "address":"Sector 18, Sector 18A Dwarka, Dwarka, New Delhi, Delhi 110075",
+    "addressLines":["Sector 18, Sector 18A Dwarka", "New Delhi, Delhi 110075"],
+    "pincode": "110075",
+    "zipCode":"110075",
+    "numBeds" : "442",
+    "rating" : "4.2",
+},
+
+
+{
+    "name": "Batra Hospital",
+    "phoneNumber":"011 2995 8747",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.5131,
+        "longitude": 77.2458,
+    },
+    "address":"1, Mehrauli - Badarpur Rd, Tughlakabad Institutional Area, Vayusenabad, New Delhi, Delhi 110062",
+    "addressLines":["1, Mehrauli - Badarpur Rd", "Tughlakabad Institutional Area, Vayusenabad", "New Delhi, Delhi 110062"],
+    "pincode": "110062",
+    "zipCode":"110062",
+    "numBeds" : "409",
+    "rating" : "3.5",
+},
+
+{
+    "name": "AIIMS Hospital",
+    "phoneNumber":"011 2658 8500",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.5659,
+        "longitude": 77.2111,
+    },
+    "address":"Ansari Nagar East, Gautam Nagar, Ansari Nagar East, New Delhi, Delhi 110029",
+    "addressLines":["Ansari Nagar East, Gautam Nagar", "Ansari Nagar East", "New Delhi, Delhi 110029"],
+    "pincode": "110029",
+    "zipCode":"110029",
+    "numBeds" : "725",
+    "rating" : "4.2",
+},
+
+
+{
+    "name": "Indraprastha Apollo Hospitals",
+    "phoneNumber":"011 7179 1090",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.541789,
+        "longitude": 77.283256,
+    },
+    "address":"Indraprastha Apollo Hospitals, Sarita Vihar, New Delhi, Delhi 110076",
+    "addressLines":["Indraprastha Apollo Hospitals", "Sarita Vihar", "New Delhi, Delhi 110076"],
+    "pincode": "110076",
+    "zipCode":"110076",
+    "numBeds" : "395",
+    "rating" : "3.7",
+},
+
+
+{
+    "name": "Human Care Medical Charitable Trust (HCMCT) Hospital",
+    "phoneNumber":"011 2508 3425",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.459383,
+        "longitude": 77.283938,
+    },
+    "address":"near telephone exchange, Palam Vihar, Sector 6 Dwarka, Dwarka, New Delhi, Delhi 110075",
+    "addressLines":["near telephone exchange, Palam Vihar", "Sector 6 Dwarka, Dwarka", "New Delhi, Delhi 110075"],
+    "pincode": "110075",
+    "zipCode":"110075",
+    "numBeds" : "250",
+    "rating" : "3.4",
+},
+
+
+{
+    "name": "Park Hospital",
+    "phoneNumber":"011 4532 3232",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.6518,
+        "longitude": 77.0932,
+    },
+    "address":"Bus Depot, Meera Enclave Outer Ring Road Near Kashopur, New Chaukhandi, Vishnu Garden, New Delhi, Delhi 110018",
+    "addressLines":["Bus Depot, Meera Enclave Outer Ring Road Near Kashopur", "New Chaukhandi", "Vishnu Garden", "New Delhi, Delhi 110018"],
+    "pincode": "110018",
+    "zipCode":"10018",
+    "numBeds" : "225",
+    "rating" : "4.4",
+},
+
+
+{
+    "name": "Hakeem Abdul Hameed Centenary Hospital",
+    "phoneNumber":"011 2605 9671",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.5151,
+        "longitude": 77.2510,
+    },
+    "address":"Guru Ravidas Marg, Block D, Hamdard Nagar, New Delhi, Delhi 110062",
+    "addressLines":["Guru Ravidas Marg, Block D", "Hamdard Nagar", "New Delhi, Delhi 110062"],
+    "pincode": "110062",
+    "zipCode":"110062",
+    "numBeds" : "200",
+    "rating" : "3.7",
+},
+
+
+{
+    "name": "Saroj Super Speciality Hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 28.7061,
+        "longitude": 77.1291,
+    },
+    "address":"Bhagawan Mahavir Marg, Near Madhuban Chowk, Block A, Sector 14, Rohini, New Delhi, Delhi 110085",
+    "addressLines":["Bhagawan Mahavir Marg", "Near Madhuban Chowk", "Block A, Sector 14", "New Delhi, Delhi 110085"],
+    "pincode": "110085",
+    "zipCode":"110085",
+    "numBeds" : "235",
+    "rating" : "3.9",
+},
+
+{
+    "name": "Satyawadi Raja Harish Chandra hospital",
+    "phoneNumber":"+918023325130",
+    "openTime" : "9am-4pm",
+    "coordinates": {
+        "latitude": 28.5025,
+        "longitude": 77.6118,
+    },
+    "address":"Satyawadi raja harish chandar hospital Campus, Narela, Delhi, 110040",
+    "addressLines":["Satyawadi raja harish chandar hospital Campus", "Narela", "Delhi, 110040"],
+    "pincode": "110040",
+    "zipCode":"110040",
+    "numBeds" : "184",
+    "rating" : "3.8",
+},
+
+{
+    "name": "Mata Chanan Devi Hospital",
+    "phoneNumber":"011 4558 2000",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 28.6188,
+        "longitude": 77.0783,
+    },
+    "address":"c1, Lal Sai Mandir Marg, Block C1, Janakpuri, New Delhi, Delhi 110058",
+    "addressLines":["c1, Lal Sai Mandir Marg, Block C1", " Janakpuri, New Delhi, Delhi 110058"],
+    "pincode": "110058",
+    "zipCode":"110058",
+    "numBeds" : "160",
+    "rating" : "2.5",
+},
+
+{
+    "name": "Mool Chand Khairati Ram Hospital lat long",
+    "phoneNumber":"011 4200 0000",
+    "openTime" : "Opens 24 Hours",
+    "coordinates": {
+        "latitude": 28.3358,
+        "longitude": 77.14798,
+    },
+    "address":" Lala Lajpat Rai Marg, Near Moolchand Metro Station, Block M, Lajpat Nagar III, Lajpat Nagar, New Delhi, Delhi 110024",
+    "addressLines":["Lala Lajpat Rai Marg, Near Moolchand Metro Station", "Block M, Lajpat Nagar III, Lajpat Nagar", "New Delhi, Delhi 110024"],
+    "pincode": "110024",
+    "zipCode":"110024",
+    "numBeds" : "140",
+    "rating" : "4.4",
+},
+
+{
+    "name": "Shanti Mukand Hospital",
+    "phoneNumber":"011 4727 6600",
+    "openTime" : "Opens 24 hours",
+    "coordinates": {
+        "latitude": 28.6457,
+        "longitude": 77.3017,
+    },
+    "address":" 2, Institutional Area, Bhartendu Harish Chandra Marg, Vikas Marg Extension, Karkardooma, New Delhi, Delhi 110092",
+    "addressLines":["2, Institutional Area, Bhartendu Harish Chandra Marg", "New Delhi, Delhi 110092"],
+    "pincode": "110092",
+    "zipCode":"110092",
+    "numBeds" : "128",
+    "rating" : "3.1",
+},
+
+{
+    "name": "Jaipur Golden Hospital",
+    "phoneNumber":"011 2790 7000",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 28.6976,
+        "longitude": 77.1096,
+    },
+    "address":"2, Naharpur Village Rd, Institutional Area, Sector 3, Rohini, Delhi, 110085",
+    "addressLines":["2, Naharpur Village Rd, Institutional Area", "Delhi, 110085"],
+    "pincode": "110085",
+    "zipCode":"110085",
+    "numBeds" : "154",
+    "rating" : "3.3",
+},
+
+{
+    "name": "Rajiv Gandhi Super Speciality Hospital",
+    "phoneNumber":"011 2289 0604",
+    "openTime" : "Opens 24 hours",
+    "coordinates": {
+        "latitude": 28.6892,
+        "longitude": 77.3165,
+    },
+    "address":"Taharpur Rd, Taharpur, Taharpur Village, Dilshad Garden, New Delhi, Delhi 110093",
+    "addressLines":["Taharpur Rd, Taharpur, Taharpur Village, Dilshad Garden", "New Delhi, Delhi 110093"],
+    "pincode": "110093",
+    "zipCode":"110093",
+    "numBeds" : "275",
+    "rating" : "3.8",
+},
+
+{
+    "name": "Sir Ganga Ram Hospital",
+    "phoneNumber":"011 2575 0000",
+    "openTime" : "Opens 24 hours",
+    "coordinates": {
+        "latitude": 28.6385,
+        "longitude": 77.1897,
+    },
+    "address":"Sarhadi Gandhi Marg, Old Rajinder Nagar, Rajinder Nagar, New Delhi, Delhi 110060",
+    "addressLines":["Sarhadi Gandhi Marg, Old Rajinder Nagar, Rajinder Nagar", "New Delhi, Delhi 110060"],
+    "pincode": "110060",
+    "zipCode":"110060",
+    "numBeds" : "195",
+    "rating" : "3.6",
+},
+
+{
+    "name": "Jayanagar General Hospital",
+    "phoneNumber":"080 2244 5711",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9262377,
+        "longitude": 77.5906121, 
+    },
+
+    "address":"32nd Cross, 4th B Block, beside Rajiv Gandhi Health Institute, Tilak Nagar, Jayanagar, Bengaluru, Karnataka 560041",
+    "addressLines":[
+        "32nd Cross, 4th B Block", 
+        "beside Rajiv Gandhi Health Institute", 
+        "Tilak Nagar, Jayanagar", 
+        "Bengaluru, Karnataka 560041"],
+    "pincode": "560041",
+    "zipCode":"56004", 
+    "numBeds" : "5",
+    "rating" : "3.6",
+},
+
+{
+    "name": "Epidemic Disease Hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9860325,
+        "longitude": 77.6420924,
+    },
+    "address":"Indira Nagar I Stage, Hoysala Nagar, Indiranagar, Bengaluru, Karnataka 560038",
+    "addressLines":["Indira Nagar I Stage", "Hoysala Nagar", "Bengaluru, Karnataka 560038"],
+    "pincode": "560038",
+    "zipCode":"56003", 
+    "numBeds" : "10",
+    "rating" : "3.4",
+},
+
+{
+    "name": "Bowring and Lady Curzon Hospital",
+    "phoneNumber":"080 2559 1325",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.982188,
+        "longitude": 77.6018685,
+    },
+    "address":"XJJ3+VJ Shivaji Nagar, Bengaluru, Karnataka 560001",
+    "addressLines":["XJJ3+VJ Shivaji Nagar", "Bengaluru, Karnataka"],
+    "pincode": "560001",
+    "zipCode":"56000", 
+    "numBeds" : "2",
+    "rating" : "3.7",
+},
+
+
+{
+    "name": "KC General Hospital",
+    "phoneNumber":"080 2334 1771",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9961428,
+        "longitude": 77.5671693,
+    },
+    "address":"Ground, Malleswaram Circle, Police Station Rd, Bengaluru, Karnataka 560003",
+    "addressLines":["Ground, Malleswaram Circle", "Police Station Rd", "Bengaluru, Karnataka 560003"],
+    "pincode": "560003",
+    "zipCode":"56000", 
+    "numBeds" : "1",
+    "rating" : "3.1",
+},
+
+{
+    "name": "SDS Tuberculosis Research Center & Rajiv Gandhi Institute of Chest Diseases",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.938365,
+        "longitude": 77.5885142,
+    },
+    "address":"Someshwaranagara Dharmaram College Post, 1st Main Rd, near NIMHANS, Bengaluru, Karnataka 560029",
+    "addressLines":["Someshwaranagara Dharmaram College Post", "1st Main Rd, near NIMHANS", "Bengaluru, Karnataka 560029"],
+    "pincode": "560029",
+    "zipCode":"56002", 
+    "numBeds" : "12",
+    "rating" : "3",
+},
+
+
+{
+    "name": "Manipal Hospitals",
+    "phoneNumber":"080 2502 4444",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9583911,
+        "longitude": 77.6466477,
+    },
+    "address":"98, HAL Old Airport Rd, Kodihalli, Bengaluru, Karnataka 560017",
+    "addressLines":["98, HAL Old Airport Rd", "Kodihalli", "Bengaluru, Karnataka 560017"],
+    "pincode": "560017",
+    "zipCode":"56001", 
+    "numBeds" : "15",
+    "rating" : "4.1",
+},
+
+
+{
+    "name": "Narayana Health City",
+    "phoneNumber":"080675 06870",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.8277197,
+        "longitude": 77.6571877,
+    },
+    "address":"258/A, Bommasandra Industrial Area Anekal Taluk, Hosur Rd, Bengaluru, Karnataka 560099",
+    "addressLines":["258/A, Bommasandra Industrial Area Anekal Taluk", "Hosur Rd", "Bengaluru, Karnataka 560099"],
+    "pincode": "560099",
+    "zipCode":"56009", 
+    "numBeds" : "20",
+    "rating" : "4.4",
+},
+
+
+{
+    "name": "Fortis Hospital",
+    "phoneNumber":"096633 67253",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.8948227,
+        "longitude": 77.5964309,
+    },
+    "address":"154, 9, Bannerghatta Main Rd, Opposite IIM, Sahyadri Layout, Panduranga Nagar, Bengaluru, Karnataka 560076",
+    "addressLines":["154, 9, Bannerghatta Main Rd", "Opposite IIM, Sahyadri Layout", "Panduranga Nagar", "Bengaluru, Karnataka 560076"],
+    "pincode": "560076",
+    "zipCode":"56007", 
+    "numBeds" : "6",
+    "rating" : "4.4",
+},
+
+
+{
+    "name": "Victoria Hospital",
+    "phoneNumber":"080 2670 1150",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9635669,
+        "longitude": 77.5716187,
+    },
+    "address":"Fort Rd, near City Market, New Tharagupet, Bengaluru, Karnataka 560002",
+    "addressLines":["Fort Rd, near City Market", "New Tharagupet", "Bengaluru, Karnataka 560002"],
+    "pincode": "560002",
+    "zipCode":"56000", 
+    "numBeds" : "5",
+    "rating" : "3.5",
+},
+
+
+{
+    "name": "Sir.C.V. Raman General Hospital",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 Hours",
+    "coordinates": {
+        "latitude": 12.9829148,
+        "longitude": 77.6428149,
+    },
+    "address":"68/1, 4th B Cross Rd, Kalyan Nagar, Michael Palaya, H Colony, Indiranagar, Bengaluru, Karnataka 560038",
+    "addressLines":["68/1, 4th B Cross Rd", "Kalyan Nagar, Michael Palaya", "H Colony, Indiranagar", "Bengaluru, Karnataka 560038"],
+    "pincode": "560038",
+    "zipCode":"56003", 
+    "numBeds" : "7",
+    "rating" : "3.8",
+},
+
+{
+    "name": "ESIC Model Hospital",
+    "phoneNumber":"+918023325130",
+    "openTime" : "9am-4pm",
+    "coordinates": {
+        "latitude": 12.9916924,
+        "longitude": 77.551712,
+    },
+    "address":"620/A/1, 12th Main Rd, 2nd Block, Rajajinagar, Bengaluru, Karnataka 560010",
+    "addressLines":["620/A/1, 12th Main Rd", "2nd Block, Rajajinagar", "Bengaluru, Karnataka 560010"],
+    "pincode": "560010",
+    "zipCode":"56001", 
+    "numBeds" : "5",
+    "rating" : "2.9",
+},
+
+{
+    "name": "General Hospital Devanahalli",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 13.2568568,
+        "longitude": 77.7252711,
+    },
+    "address":"SH 96, Devanahalli, Binnamangala, Karnataka 562110",
+    "addressLines":["SH 96, Devanahalli", "Binnamangala, Karnataka 562110"],
+    "pincode": "562110",
+    "zipCode":"56211", 
+    "numBeds" : "4",
+    "rating" : "2.8",
+},
+
+{
+    "name": "BGS Global Institute of Medical Sciences",
+    "phoneNumber":"+918026984802",
+    "openTime" : "Temporarily closed",
+    "coordinates": {
+        "latitude": 12.9022857,
+        "longitude": 77.4956326,
+    },
+    "address":"No. 67, BGS Health & Education City, Uttarahalli Road, Kengeri, Bengaluru, Karnataka 560060",
+    "addressLines":["No. 67, BGS Health & Education City", "Uttarahalli Road, Kengeri", "Bengaluru, Karnataka 560060"],
+    "pincode": "560060",
+    "zipCode":"56006", 
+    "numBeds" : "7",
+    "rating" : "4.1",
+},
+
+{
+    "name": "Rajarajeshwari Medical College and Hospital",
+    "phoneNumber":"+918028437444",
+    "openTime" : "Temporatily closed",
+    "coordinates": {
+        "latitude": 12.8961554,
+        "longitude": 77.4596629,
+    },
+    "address":"202, Mysore Road Kambipura, Bengaluru, Karnataka 560074",
+    "addressLines":["202, Mysore Road Kambipura", "Bengaluru, Karnataka 560074"],
+    "pincode": "560074",
+    "zipCode":"56007", 
+    "numBeds" : "7",
+    "rating" : "3.6",
+},
+
+{
+    "name": "General Hospital,Nelamangala",
+    "phoneNumber":"Not Available",
+    "openTime" : "Open 24 hours",
+    "coordinates": {
+        "latitude": 13.0987354,
+        "longitude": 77.389022,
+    },
+    "address":"Jayanagara, Indiranagara, Karnataka 562123",
+    "addressLines":["Jayanagara, Indiranagara", "Karnataka 562123"],
+    "pincode": "562123",
+    "zipCode":"56212", 
+    "numBeds" : "5",
+    "rating" : "2.8",
+},
+
+{
+    "name": "General Hospital, Doddaballapur",
+    "phoneNumber":"Not Available",
+    "openTime" : "Not Available",
+    "coordinates": {
+        "latitude": 13.2843621,
+        "longitude": 77.5292349,
+    },
+    "address":"7GVV+2J Doddaballapura, Karnataka 561203",
+    "addressLines":["7GVV+2J Doddaballapura", "Karnataka 561203"],
+    "pincode": "561203",
+    "zipCode":"56120", 
+    "numBeds" : "4",
+    "rating" : "4.2",
+},
+
+{
+    "name": "General Hospital, Anekal",
+    "phoneNumber":"Not Available",
+    "openTime" : "9am-4:30pm",
+    "coordinates": {
+        "latitude": 12.7575095,
+        "longitude": 77.6367502,
+    },
+    "address":"Weaver Colony, Anekal, Karnataka 562106",
+    "addressLines":["Weaver Colony, Anekal", "Karnataka 562106"],
+    "pincode": "562106",
+    "zipCode":"56210", 
+    "numBeds" : "6",
+    "rating" : "4.1",
+},
+]
